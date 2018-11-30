ced02032267b9e6bb0efd4a40c9d2100b7ed55ca
Daniel Segovia <segoviadaniel2@gmail.com>
2018-11-30 12:04:27 -0300

    Easy

---
+++ 12478 - Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+
+#define debug(x) cout << (#x) << " tiene el valor " << (x) << endl
+
+int main(void)
+{
+
+	cout << "KABIR" << endl;
+
+	return 0;
+}
